fa8479df558a0780c4f4bda58f7e3711b63baf15
Ritesh Reddy <ritesh_r_reddy@hotmail.com>
2016-09-11 12:43:51 +0100

    Support for user defined page breaks. Randomly selected backgrounds for slides.

--- ppt/Presentation.py
+++ ppt/Presentation.py
@@ -8,6 +8,7 @@ from pptx.enum.text import MSO_ANCHOR, MSO_AUTO_SIZE
 from datetime import date
 import os
 from flaskappbase import app
+import random
 
 class SaiPresentation():
     def __init__(self, title_silde_image = os.path.join(app.config['DATA_DIRECTORY'], "title_slide.jpg")):
@@ -41,9 +42,9 @@ class SaiPresentation():
         return text_box
 
     @staticmethod
-    def add_run_to_slide_with_font(slide, left, top, width, height, text_size, alignment = PP_ALIGN.CENTER, color = RGBColor(0x00, 0x00, 0x00), bold = False, italic = None):
+    def add_run_to_slide_with_font(slide, left, top, width, height, text_size, alignment = PP_ALIGN.CENTER, color = RGBColor(0xff, 0xff, 0xff), bold = False, italic = None):
         text_box = SaiPresentation.add_textBox(slide, left, top, width, height)
-        para = SaiPresentation.add_paragraph_with_alignment(text_box)
+        para = SaiPresentation.add_paragraph_with_alignment(text_box, alignment)
         run = SaiPresentation.add_new_run_with_text(para, "")
         SaiPresentation.set_run_font(run, text_size, color, bold, italic)
         return run
@@ -80,15 +81,18 @@ class SaiPresentation():
         :return:
         """
         MAX_ROW_LENGTH = 49
-        MAX_ROW_COUNT = 11
-        def bhajan_slide_template():
-            slide = self.prs.slides.add_slide(self.prs.slide_layouts[5])
-            bhajan_rn = self.add_run_to_slide_with_font(slide,Inches(0.5), Inches(1.5), Inches(9), Inches(5.5), Pt(26))
+        MAX_ROW_COUNT = 9
+        def bhajan_slide_template(background_path=None):
+            slide = self.prs.slides.add_slide(self.prs.slide_layouts[6])
+            if not background_path is None:
+                slide.shapes.add_picture(background_path, Inches(0), Inches(0), height=Inches(7.5), width=Inches(10))
+            title_rn = self.add_run_to_slide_with_font(slide, Inches(0.5), Inches(0.25), Inches(9), Inches(1), Pt(36))
+            bhajan_rn = self.add_run_to_slide_with_font(slide,Inches(0.5), Inches(1.25), Inches(9), Inches(5.5), Pt(32))
             key_rn = self.add_run_to_slide_with_font(slide,Inches(0.5), Inches(6.5), Inches(1), Inches(0.75), Pt(16), PP_ALIGN.LEFT)
             next_bhajan_name_rn = self.add_run_to_slide_with_font(slide, Inches(2.5), Inches(6.5), Inches(4), Inches(0.75), Pt(16))
             next_key_rn = self.add_run_to_slide_with_font(slide, Inches(8), Inches(6.5), Inches(1), Inches(0.75), Pt(16), PP_ALIGN.RIGHT)
 
-            return slide, bhajan_rn, key_rn, next_bhajan_name_rn, next_key_rn
+            return slide, title_rn, bhajan_rn, key_rn, next_bhajan_name_rn, next_key_rn
 
         def get_true_lines(text):
             line_list = text.split("\n")
@@ -108,21 +112,28 @@ class SaiPresentation():
                     line_list[ind] = new_line
             return '\n'.join(line_list)
 
-        def get_text_per_slide(text):
-            slide_text = []
-            lines = text.split("\r\n")
-            this_set = []
-            for i in range(0, len(lines), MAX_ROW_COUNT):
-                for j in range(i, min(len(lines), i+MAX_ROW_COUNT)):
-                    this_set.append(lines[j])
-                slide_text.append('\n'.join(this_set))
+
+
+        def handle_user_defined_pagebreaks(text):
+            def get_text_per_slide(text):
+                lines = text.split("\r\n")
                 this_set = []
+                for i in range(0, len(lines), MAX_ROW_COUNT):
+                    for j in range(i, min(len(lines), i+MAX_ROW_COUNT)):
+                        this_set.append(lines[j])
+                    slide_text.append('\n'.join(this_set))
+                    this_set = []
+            PAGE_BREAK_MARKER = '\r\n[pagebreak]\r\n'
+            slide_text = []
+            lines = text.split(PAGE_BREAK_MARKER)
+            map(get_text_per_slide, lines)
             return slide_text
 
-        def add_a_bhajan_slide(bhajan_name, text, final = True, key="", next_bhajan_name="", next_key=""):
-            slide, bhajan_rn, key_rn, nxt_bhajan_rn, nxt_key_rn = bhajan_slide_template()
-            slide.shapes.title.text = bhajan_name
-            bhajan_rn.text = text
+
+        def add_a_bhajan_slide(bhajan_name, text, final = True, key="", next_bhajan_name="", next_key="", background_path=None):
+            slide, title_rn, bhajan_rn, key_rn, nxt_bhajan_rn, nxt_key_rn = bhajan_slide_template(background_path)
+            title_rn.text = bhajan_name
+            bhajan_rn.text = text.strip()
             key_rn.text = key
             if not final:
                 nxt_bhajan_rn.text = "Continued"
@@ -130,8 +141,13 @@ class SaiPresentation():
                 nxt_bhajan_rn.text = next_bhajan_name
                 nxt_key_rn.text = next_key
 
+        files = os.listdir(os.path.join(app.config['DATA_DIRECTORY'], 'backgrounds'))
+        background_path = None
+        if len(files) != 0:
+            index = random.randint(0, len(files) - 1)
+            background_path = os.path.join(os.path.join(app.config['DATA_DIRECTORY'], 'backgrounds'), files[index])
 
-        text_per_slide = get_text_per_slide(get_true_lines(bhajan_txt))
+        text_per_slide = handle_user_defined_pagebreaks(get_true_lines(bhajan_txt))
         for pos, text in enumerate(text_per_slide, start = 1):
             if pos == len(text_per_slide):
                 final = True
@@ -140,7 +156,7 @@ class SaiPresentation():
             if len(bhajan_txt) == 0: # Empty Bhajan
                 self.add_full_image_slide(os.path.join(app.config['DATA_DIRECTORY'], 'filler.jpg'))
             else:
-                add_a_bhajan_slide(bhajan_name, text, final, key, next_bhajan_name, next_key)
+                add_a_bhajan_slide(bhajan_name, text, final, key, next_bhajan_name, next_key, background_path)
 
     def save_presentation(self, filename):
         self.prs.save(filename)
