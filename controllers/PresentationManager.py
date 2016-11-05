355d43dc0b447d4ca3dbdea8564f3fcb6f99d964
Ritesh Reddy <ritesh_r_reddy@hotmail.com>
2016-11-05 18:50:41 +0000

    Removed validation of bhajans in system during presentation time

--- controllers/PresentationManager.py
+++ controllers/PresentationManager.py
@@ -24,28 +24,29 @@ def generate_presentation_experimental():
     subtitle = form_dict['subtitle'][0] # form_dict contains an list, get the first element
     presentation = SaiPresentation(title, subtitle)
     if not 'bhajan_id' in form_dict:
+        # no bhajans were selected, going beyond is troublesome so just exit here.
         filename = 'SaiBhajans_' + datetime.today().strftime("%d_%m_%Y_%H_%M_%S") + '.pptx'
         presentation.save_presentation(os.path.join(app.config['POWERPOINT_OUTPUT'], "bhajans.pptx"))
         return send_from_directory(app.config['POWERPOINT_OUTPUT'], "bhajans.pptx", as_attachment=True,
                                attachment_filename=filename)
     bhajan_ids = form_dict['bhajan_id']
+    title = form_dict['title']
     bhajan_text_adjusted = form_dict['bhajan']
     keys = form_dict['key']
-    bhajans = BhajanModel.get_bhajans_if_they_exist(bhajan_ids)
-    together = zip(bhajans, keys, bhajan_text_adjusted)
+    together = zip(title, keys, bhajan_text_adjusted)
 
     for index in range(len(together) - 1):
-        current = together[index]  # [bhajan, key, bhajan_text_adjusted]
-        next = together[index + 1]  # [bhajan, key, bhajan_text_adjusted]
-        bhajan_name = current[0]['name']
-        bhajan_txt = current[2] #current[0]['bhajan']
+        current = together[index]  # [title, key, bhajan_text_adjusted]
+        next = together[index + 1]  # [title, key, bhajan_text_adjusted]
+        bhajan_name = current[0]
+        bhajan_txt = current[2]
         bhajan_key = current[1]
-        next_bhajan_name = next[0]['name']
+        next_bhajan_name = next[0]
         next_bhajan_key = next[1]
         presentation.add_bhajan_slide(bhajan_name, bhajan_txt, bhajan_key, next_bhajan_name, next_bhajan_key)
 
-    last = together[-1]  # [bhajan, key, bhajan_text_adjusted]
-    bhajan_name = last[0]['name']
+    last = together[-1]  # [title, key, bhajan_text_adjusted]
+    bhajan_name = last[0]
     bhajan_txt = last[2] #last[0]['bhajan']
     bhajan_key = last[1]
     presentation.add_bhajan_slide(bhajan_name, bhajan_txt, bhajan_key)
